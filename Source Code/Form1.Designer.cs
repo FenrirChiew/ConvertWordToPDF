3047f97fddbea5a604556c19d93275c668d066c7
FenrirChiew <86295813+FenrirChiew@users.noreply.github.com>
2022-08-29 07:52:06 -0700

    Add files via upload

---
+++ Source Code/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace ConvertWordToPDF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.heading1 = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.textPath = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // heading1
+            // 
+            this.heading1.AutoSize = true;
+            this.heading1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.heading1.Location = new System.Drawing.Point(213, 55);
+            this.heading1.Name = "heading1";
+            this.heading1.Size = new System.Drawing.Size(380, 38);
+            this.heading1.TabIndex = 0;
+            this.heading1.Text = "Word to PDF Converter";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnBrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowse.ForeColor = System.Drawing.Color.Black;
+            this.btnBrowse.Location = new System.Drawing.Point(213, 300);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(85, 25);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = false;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.ForeColor = System.Drawing.Color.Black;
+            this.btnConvert.Location = new System.Drawing.Point(520, 300);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(85, 25);
+            this.btnConvert.TabIndex = 3;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = false;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // textPath
+            // 
+            this.textPath.Location = new System.Drawing.Point(213, 190);
+            this.textPath.Name = "textPath";
+            this.textPath.Size = new System.Drawing.Size(392, 22);
+            this.textPath.TabIndex = 4;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textPath);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.heading1);
+            this.Name = "Form1";
+            this.Text = "Word To PDF Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label heading1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox textPath;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Source Code/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using SautinSoft.Document;
+
+namespace ConvertWordToPDF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = @"DOCX Files (*.docx) | *.docx";
+            openFileDialog1.FileName = "";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textPath.Text = System.IO.Path.GetFullPath(openFileDialog1.FileName);
+            }
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            var wordFilename = textPath.Text;
+            var pdfFilename = Path.GetFullPath(textPath.Text.Replace(".docx", ".pdf"));
+
+            DocumentCore dc = DocumentCore.Load(wordFilename);
+            dc.Save(pdfFilename);
+
+            MessageBox.Show(pdfFilename + " generated successfully.");
+        }
+    }
+}
